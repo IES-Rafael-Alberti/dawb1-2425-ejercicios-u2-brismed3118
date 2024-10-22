58d1067ac5bd970b4ab5684c28022253e3b10c98
brismed3118 <rmbenito@iesrafaelalberti.es>
2024-10-22 18:04:10 +0200

    V22/10 commit ejercicios hasta el 8

--- src/ej2.py
+++ src/ej2.py
@@ -2,9 +2,21 @@
 #   SIN TERMINAR
 #
 
+def comprueba_contrasenia(pswd:str, entrada:str) -> bool:
+    
+    if pswd == entrada:
+        return True
+    else:
+        return False
+    
 def main():
-   pswd = input("Introduce tu contraseña: ").lower()
-   print (pswd)
+   pswd = "contraseña"
+   entrada = input("Introduzca la contraseña > ").lower()
+   if comprueba_contrasenia(pswd, entrada) == True:
+       print("Contraseña correcta")
+   else:
+       print("Contraseña incorrecta...")
+       
 
 
 if __name__ == "__main__":
--- src/ej5.py
+++ src/ej5.py
@@ -2,7 +2,29 @@
 # 
 #
 
+def tributa(edad: int, salario: int) -> bool:
+    if edad > 16 and salario >= 1000:
+        return True
+    else:
+        return False
+
+
 def main():
+    edad = None
+    salario = None
+
+    while edad == None or salario == None:
+        try:
+            edad =  int(input("Introduzca su edad: "))
+            salario = int(input("Introduzca su salario mensual: "))
+        except ValueError:
+            print("**ERROR** Edad o salario no válidos")
+
+    if tributa(edad, salario) == True:
+        print("Tributa")
+    else:
+        print("No tributa")
+
     pass
 
 if __name__ == "__main__":
