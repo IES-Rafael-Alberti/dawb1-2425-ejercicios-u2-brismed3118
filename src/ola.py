528f413bb9b6d5d3e57fe3b6de45065971e8f810
brismed3118 <rmbenito@iesrafaelalberti.es>
2024-10-25 17:04:05 +0200

    V24/10 commit ej2_08 y renombrar hechos

---
+++ src/ola.py
@@ -0,0 +1,17 @@
+import time
+import os
+import shutil
+import random
+
+def ola():   
+    i = None
+
+def main():
+    i = None
+
+    while i == None:
+        ola()
+        
+  
+if __name__ == "__main__":
+    main()
